5d765510fbb4b2e6149a611ac3dfb261e4c63dcc
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-21 17:26:23 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,6 @@
 name = gets.chomp
 
-def greeting
+def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 puts "Please enter your name so that we can greet you more personally:"
